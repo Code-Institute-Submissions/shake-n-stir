be050207e85a3a6042ac17883fcfb7fbb0c99469
TomC2311 <tomaslcripps@gmail.com>
2021-04-22 13:11:41 +0000

    Update user password requirements

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def register():
 
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
-        
+
         if password != confirm_password:
             flash("Please ensure that your passwords match.")
             return redirect(url_for("register"))
@@ -59,26 +59,23 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if user exists in database
+        # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # check if user password matches their registered password
+            # ensure hashed password matches user input
             if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for(
-                            "profile", username=session["user"]))
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
             else:
-                # if user inputs incorrect password
+                # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            # if user inputs incorrect username
+            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
