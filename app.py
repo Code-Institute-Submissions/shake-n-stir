f627a4029bc6052080f0967ae0bb37a4f508c747
TomC2311 <tomaslcripps@gmail.com>
2021-04-22 14:05:04 +0000

    Fix login page password error

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+                existing_user["password"], request.form.get("password").lower()):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
             else:
@@ -78,7 +78,7 @@ def login():
             # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-
+   
     return render_template("login.html")
 
 
