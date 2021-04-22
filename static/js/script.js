34a75b2a2c18c41a5e7eacf16452d1d4b8503d6a
TomC2311 <tomaslcripps@gmail.com>
2021-04-22 09:46:30 +0000

    Add JavaScript validation to register form

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+/* --Bootstrap form validation-- */
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.getElementsByClassName('needs-validation');
+
+    // Loop over them and prevent submission
+    let validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+$('#password, #confirm_password').on('keyup', function () {
+  if ($('#password').val() == $('#confirm_password').val()) {
+    $('#confirm_password').addClass("is-valid").removeClass("is-invalid");
+  
+  } else 
+      $('#confirm_password').removeClass("is-valid").addClass("is-invalid");
+});
