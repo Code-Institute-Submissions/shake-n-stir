47ac4972ec855bac9cbc6ec8d6e679c1a2c0d64d
TomC2311 <tomaslcripps@gmail.com>
2021-04-23 06:01:20 +0000

    Add Profile template and session user profile

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for('profile', username=session["user"]))
     return render_template("register.html")
 
 
@@ -66,9 +67,12 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password").lower()):
+                existing_user["password"], request.form.get(
+                    "password").lower()):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(
+                        url_for('profile', username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -78,8 +82,19 @@ def login():
             # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
-   
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grabs the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
