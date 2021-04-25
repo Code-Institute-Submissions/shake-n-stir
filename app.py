517ec6c13d90831f44a5a49e58d4df12a7ef0838
TomC2311 <tomaslcripps@gmail.com>
2021-04-25 11:23:51 +0000

    Link edit button to correct cocktails via ObjectId

--- app.py
+++ app.py
@@ -138,6 +138,15 @@ def add_cocktail():
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_cocktail.html", categories=categories)
 
+
+@app.route("/edit_cocktail/<cocktail_id>", methods=["GET", "POST"])
+def edit_cocktail(cocktail_id):
+    cocktail = mongo.db.cocktails.find_one({"_id": ObjectId(cocktail_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template(
+        "edit_cocktail.html", cocktail=cocktail, categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
