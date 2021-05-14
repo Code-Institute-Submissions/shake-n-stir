7c3479f19d7a4c8a4b6d752aa45606542136eceb
TomC2311 <tomaslcripps@gmail.com>
2021-05-14 13:04:33 +0000

    Add functionality to redirect non users to login page when trying access a profile

--- app.py
+++ app.py
@@ -118,6 +118,9 @@ def login():
 # user profile
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    # redirects none users to login if attempted to go to a user's profile
+    if session.get("user") is None:
+        return redirect(url_for("login"))
     # grabs the session user's username from the database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
